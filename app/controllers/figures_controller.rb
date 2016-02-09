46562fdbf80b7304d6adeeaa0c9dda0d250cf5ea
Kevin McCormack <wittyman37@yahoo.com>
2016-02-09 15:32:31 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,41 +9,29 @@ class FiguresController < ApplicationController
   get '/figures/new' do
     @landmarks = Landmark.all
     @titles = Title.all
-    #binding.pry
     erb :new
   end
 
   post '/figures' do
-
     figure = Figure.create(name: params[:figure]["name"])
 
-    #if params[:figure]["title_ids"] != nil
-      #title_ids = params[:figure]["title_ids"].collect {|id| id.to_i}
-      #title_ids.each { |id| figure.titles << Title.find_by_id(id)}
-    #  params[:figure]["title_ids"].each { |id| figure.titles << Title.find_by_id(id.to_i)}
-    #end
+    # add checked titles
     params[:figure]["title_ids"].each {
       |id| figure.titles << Title.find_by_id(id.to_i)
     } unless params[:figure]["title_ids"] == nil
 
-    #if params[:figure]["landmark_ids"] != nil
+    # add checked landmarks
     params[:figure]["landmark_ids"].each {
       |id| figure.landmarks << Landmark.find_by_id(id.to_i)
     } unless params[:figure]["landmark_ids"] == nil
-      #landmark_ids = params[:figure]["landmark_ids"].collect {|id| id.to_i}
-      #landmark_ids.each { |id| figure.landmarks << Landmark.find_by_id(id)}
-      #figure.landmarks = landmark_ids
-    #end
-    #binding.pry
-    #figure.landmarks = params[:figure]["landmark_ids"] unless params[:figure]["landmark_ids"] == nil
 
+    # create new title and add it to figures
     if params[:title]["name"] != ""
-      #binding.pry
       new_title = Title.find_or_create_by(name: params[:title]["name"])
       figure.titles << new_title
-
     end
 
+    # create new landmark and add it to figures
     if params[:landmark]["name"] != ""
       new_landmark = Landmark.find_or_create_by(name: params[:landmark]["name"])
       new_landmark.year_completed = params[:landmark]["year_completed"]
@@ -58,8 +46,34 @@ class FiguresController < ApplicationController
   patch '/figures/:id' do
     figure = Figure.find_by_id(params[:id])
     figure.name = params[:figure]["name"]
-    figure.year_completed = params[:figure]["year_completed"]
+    figure.titles = []
+    figure.landmarks = []
+
+    # add checked titles
+    params[:figure]["title_ids"].each {
+      |id| figure.titles << Title.find_by_id(id.to_i)
+    } unless params[:figure]["title_ids"] == nil
+
+    # add checked landmarks
+    params[:figure]["landmark_ids"].each {
+      |id| figure.landmarks << Landmark.find_by_id(id.to_i)
+    } unless params[:figure]["landmark_ids"] == nil
+
+    # create new title and add it to figures
+    if params[:title]["name"] != ""
+      new_title = Title.find_or_create_by(name: params[:title]["name"])
+      figure.titles << new_title
+    end
+
+    # create new landmark and add it to figures
+    if params[:landmark]["name"] != ""
+      new_landmark = Landmark.find_or_create_by(name: params[:landmark]["name"])
+      new_landmark.year_completed = params[:landmark]["year_completed"]
+      figure.landmarks << new_landmark
+    end
+
     figure.save
+
     redirect to "/figures/#{figure.id}"
   end
 
@@ -71,6 +85,8 @@ class FiguresController < ApplicationController
 
   get '/figures/:id/edit' do
     @figure = Figure.find_by_id(params[:id])
+    @landmarks = Landmark.all
+    @titles = Title.all
     erb :edit
   end
 end
